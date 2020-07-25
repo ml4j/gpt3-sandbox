4a382f0d2d8ee1183a1ff77737ff3b778e9de32b
Michael Lavelle <michael@lavelle.name>
2020-07-25 22:53:03 +0100

    Configuring run_java_app.py to use JSON template and prompt containing prompt configuration, downloaded from a github url

--- examples/run_java_app.py
+++ examples/run_java_app.py
@@ -5,10 +5,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from api import GPT, Example, UIConfig
 from api import demo_web_app
 
-question_prefix = 'What does the code below do? Write one comment for each of the following numbered statements:\n\n'
-question_suffix = "\n\n"
-answer_prefix = "Comments:\n\n"
-answer_suffix = "\n"
+prompt_example_url ='https://raw.githubusercontent.com/ml4j/gtp-3-prompt-templates/master/question-answer/code-explanation/examples/java-example-1/code_explanation_example_prompt_1.json'
+template_url = 'https://raw.githubusercontent.com/ml4j/gtp-3-prompt-templates/master/question-answer/code-explanation/templates/code_explanation_template_1.json'
+
+import requests
+import json
+
+prompt_example_json = json.loads(requests.get(prompt_example_url).text)
+template_json = json.loads(requests.get(template_url).text)
+
+question_prefix = template_json['questionPrefix']
+question_suffix = template_json['questionSuffix']
+answer_prefix = template_json['answerPrefix']
+answer_suffix = template_json['answerSuffix']
 
 # Construct GPT object and show some examples
 gpt = GPT(engine="davinci",
@@ -20,24 +29,14 @@ gpt = GPT(engine="davinci",
           output_suffix = answer_suffix,
           append_output_prefix_to_query = True)
 
-gpt.add_example(Example('Statement 1) stream().map(Dimension::getName).collect(Collectors.toList()).toString();\n' +
-'Statement 2) stream().flatMap(c -> c.decompose().stream()).collect(Collectors.toList());\n' +
-'Statement 3) allDecomposedAliases.add(decompose().stream().collect(Collectors.toList()));\n' +
-'Statement 4) box.getScaledCorners(originalImage.getWidth(), originalImage.getHeight()));\n' +
-'Statement 5) List<Image> subImages = channelConcatImages.subList(channelRangeStart, channelRangeEnd);',
-'Statement 1) // Obtain the names of the dimensions, collect them into a list, and return the list as a string.\n' +
-'Statement 2) // Call decompose recursively on each of the elements of the stream and collect the results into a list\n' +
-'Statement 3) // Collect the aliases from the stream returned by the decompose() method into a list, and add them to allDecomposedAliases\n' +
-'Statement 4) // Get the scaled corners of a bounding box using its width and height of the original image\n' +
-'Statement 5) // Obtain a list of the elements of subImages with indexes been channelRangeStart (inclusive) and channelRangeEnd (exclusive) and assign the list to a local variable called subImages'))
 
+for example in prompt_example_json['questionsAndAnswers']:
+    gpt.add_example(Example(example['question'], example['answer']))
 
 # Define UI configuration
 config = UIConfig(description="Code to comment",
                   button_text="Translate",
-                  placeholder="Statement 1) List<User> friendsList = me.getFriends();\n"
-"Statement 2) List<User> genXFriends = friendsList.stream().filter(user.getAge() > 40 user.getAge() <= 55).collect(Collectiors.toList());\n" +
-"Statement 3) genXFriends.forEach(user -> user.sendMessage(\"Hi \" + user.getScreenName() + \"you are the best!\");")
+                  placeholder=prompt_example_json['defaultPromptQuestion'])
 
 
 demo_web_app(gpt, config)
