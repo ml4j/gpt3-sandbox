cba59b07b680ca51c7f7f10d40e484a1e5644bbf
Michael Lavelle <michael@lavelle.name>
2020-07-26 13:20:30 +0100

    Changes in line with refactoring

--- examples/run_java_app.py
+++ examples/run_java_app.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from api import GPT, Example, UIConfig
+from api import GPT, Example, TextFormat, QueryFormat, UIConfig
 from api import demo_web_app
 
 prompt_example_url ='https://raw.githubusercontent.com/ml4j/gtp-3-prompt-templates/master/question-answer/code-explanation/examples/java-example-1/code_explanation_example_prompt_1.json'
@@ -14,21 +14,16 @@ import json
 prompt_example_json = json.loads(requests.get(prompt_example_url).text)
 template_json = json.loads(requests.get(template_url).text)
 
-question_prefix = template_json['questionPrefix']
-question_suffix = template_json['questionSuffix']
-answer_prefix = template_json['answerPrefix']
-answer_suffix = template_json['answerSuffix']
+query_format = QueryFormat(
+    TextFormat(template_json['questionPrefix'], template_json['questionSuffix']),
+    TextFormat(template_json['answerPrefix'], template_json['answerSuffix']),
+    True)
 
 # Construct GPT object and show some examples
 gpt = GPT(engine="davinci",
           temperature=0.1,
           max_tokens=100,
-          input_prefix = question_prefix,
-          input_suffix = question_suffix,
-          output_prefix = answer_prefix,
-          output_suffix = answer_suffix,
-          append_output_prefix_to_query = True)
-
+          query_format = query_format)
 
 for example in prompt_example_json['questionsAndAnswers']:
     gpt.add_example(Example(example['question'], example['answer']))
